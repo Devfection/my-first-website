d709d00fdda371deb5783c22c6ed84fee5343d30
Devfection <8330@cygnific.com>
2026-05-22 14:46:06 +0200

    Created the files on github

---
+++ src/index.js
@@ -0,0 +1,66 @@
+
+function citySearch(event){
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let city = searchInputElement.value;
+  let apiKey = "61ff3ct11d507f83e0fcob41d8ed7a85";
+  let units = "metric";
+  let weatherApi = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`; 
+  axios.get(weatherApi).then(displayTemperature);
+}
+function displayTemperature(response){
+    console.log(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#current-temperature-value");
+    let temperature = Math.round(response.data.temperature.current);
+    temperatureElement.innerHTML=temperature;
+
+    let searchInputElement = document.querySelector("#search-input");
+    let newCity = document.querySelector("#current-city");
+    newCity.innerHTML = searchInputElement.value;
+}
+
+ 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", citySearch);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
+//let city = "Sydney";
+//let apiKey = "61ff3ct11d507f83e0fcob41d8ed7a85";
+//let units = "metric";
+//let weatherApi = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`; 
+
+
+//axios.get(weatherApi).then(displayTemperature);
